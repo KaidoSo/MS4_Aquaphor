423b98b75d02ffba17f1647e28752d15eba27d82
KaidoSo <kaido.soomlais@europaskolan.nu>
2020-06-27 18:24:28 +0200

    Added home and contacts html pages

--- aquaphor/store/views.py
+++ aquaphor/store/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'home.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
 # Create your views here.
