0c939c903b573de13b3c737a7c79468150f58d2b
KaidoSo <kaido.soomlais@europaskolan.nu>
2020-08-23 12:46:41 +0200

    added grid of products to home and some dummy pics

--- aquaphor/store/views.py
+++ aquaphor/store/views.py
@@ -5,6 +5,14 @@ def home(request):
     return render(request, 'home.html')
 
 
+def shop(request):
+    return render(request, 'shop.html')
+
+
+def productPage(request):
+    return render(request, 'product.html')
+
+
 def contacts(request):
     return render(request, 'contacts.html')
 # Create your views here.
