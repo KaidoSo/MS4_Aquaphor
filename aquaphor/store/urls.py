15d16b3a7070275b6fd4609fa3766ce1a993aa64
KaidoSo <kaido.soomlais@europaskolan.nu>
2020-08-23 13:56:46 +0200

    added product page and it's css

--- aquaphor/store/urls.py
+++ aquaphor/store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/', views.productPage, name='product'),
 ]
