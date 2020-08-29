c2ea215b182e665d365776d4fbdc041e23d79a7c
KaidoSo <kaido.soomlais@europaskolan.nu>
2020-08-29 19:45:54 +0200

    tried moving folders so that requriements is found

--- aquaphor/settings.py
+++ aquaphor/settings.py
@@ -28,10 +28,7 @@ SECRET_KEY = config('DB_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = [
-    os.environ.get('HEROKU_HOSTNAME'),
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['kaidoso-ms4-aquaphor.herokuapp.com', 'localhost']
 
 
 # Application definition
