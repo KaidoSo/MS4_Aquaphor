2b62ebaf7200f0bb5ae579655ea3d9a144ed4096
KaidoSo <kaido.soomlais@europaskolan.nu>
2020-08-30 12:53:53 +0200

    added order model and adjusted admin

--- aquaphor/store/models.py
+++ aquaphor/store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+#Model: Category
+
 
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
@@ -20,6 +22,7 @@ class Category(models.Model):
         return self.name
 
 
+#Model: Product
 class Product(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
@@ -44,6 +47,7 @@ class Product(models.Model):
         return self.name
 
 
+#Model: Cart
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
@@ -67,6 +71,50 @@ class CartItem(models.Model):
 
     def sub_total(self):
         return self.product.price * self.quantity
+
+    def __str__(self):
+        return self.product
+
+
+#Model: Order
+class Order(models.Model):
+    token = models.CharField(max_length=250, blank=True)
+    total = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name='EUR Order Total')
+    emailAddress = models.EmailField(
+        max_length=250, blank=True, verbose_name='Email Address')
+    created = models.DateTimeField(auto_now_add=True)
+    billingName = models.CharField(max_length=250, blank=True)
+    billingAddress1 = models.CharField(max_length=250, blank=True)
+    billingCity = models.CharField(max_length=250, blank=True)
+    billingPostcode = models.CharField(max_length=250, blank=True)
+    billingCountry = models.CharField(max_length=250, blank=True)
+    shippingName = models.CharField(max_length=250, blank=True)
+    shippingAddress1 = models.CharField(max_length=250, blank=True)
+    shippingCity = models.CharField(max_length=250, blank=True)
+    shippingPostcode = models.CharField(max_length=250, blank=True)
+    shippingCountry = models.CharField(max_length=250, blank=True)
+
+    class Meta:
+        db_table = 'Order'
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name='EUR Price')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'OrderItem'
+
+    def sub_total(self):
+        return self.quantity * self.price
 
     def __str__(self):
         return self.product
